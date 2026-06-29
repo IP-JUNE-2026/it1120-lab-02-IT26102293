b666824b09f5752d964c3df8962a588f237f3638
IT26102293 <it26102293@my.sliit.lk>
2026-06-29 23:02:10 +0530

    Add files via upload

---
+++ Q1/IT26102293Lab2Q1.java
@@ -0,0 +1,16 @@
+public class  IT26102293Lab2Q1 {
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+        double length, width;
+
+        // Width = 3/4 of Length
+        // Perimeter = 2 × (length + width)
+
+        length = perimeter / 3.5;
+        width = (3.0 / 4.0) * length;
+
+        System.out.println("Length = " + length);
+        System.out.println("Width = " + width);
+    }
+} 
