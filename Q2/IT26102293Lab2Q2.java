3833ebe5c2c1d717d86eb5f3613f4bd9fbfa0ce1
IT26102293 <it26102293@my.sliit.lk>
2026-06-29 23:17:34 +0530

    Lab2 to Q2

---
+++ Q2/IT26102293Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26102293Lab2Q2 {
+    public static void main(String[] args) {
+
+        double side = 10;
+        double pi = 3.14;
+
+        double ropeLength = 4 * side;
+        double radius = ropeLength / (2 * pi);
+
+        System.out.println("Radius = " + radius);
+    }
+}
